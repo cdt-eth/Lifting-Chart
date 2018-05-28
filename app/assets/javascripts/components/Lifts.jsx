6ef7e2ac8878aa5cd8a4cecbd8baade15f4e428d
christian <christiandavidphoto@gmail.com>
2018-05-28 18:10:41 -0400

    statically render data

--- app/assets/javascripts/components/Lifts.jsx
+++ app/assets/javascripts/components/Lifts.jsx
@@ -4,45 +4,68 @@ class Lifts extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {
+    state = {
       lifts: props.data
-    };
-
-    Lifts.defaultProps = {
-      lifts: []
     };
   }
 
-  // getInitialState = () => {
-  //   lifts: this.props.data;
-  // };
-  // getDefaultProps = () => {
-  //   lifts: [];
-  // };
+  // getInitialState() {
+  //   return {
+  //     lifts: this.props.data
+  //   };
+  // }
 
   render() {
+    let lifts = this.props.data;
     return (
       <div className="lifts">
         <h1 className="title"> Lifts </h1>
         <table className="table table-bordered">
           <thead>
-            <tr />
-            <th> Date </th>
-            <th> Lift Name </th>
-            <th> Weight Lifted </th>
-            <th> Reps Performed </th>
-            <th> 1 RM </th>
+            <tr>
+              <th> Date </th>
+              <th> Lift Name </th>
+              <th> Weight Lifted </th>
+              <th> Reps Performed </th>
+              <th> 1 RM </th>
+            </tr>
           </thead>
-          {/* <tbody>
-            {lift.map(lift => {
-              return <li key={lift.id}> {lift} </li>;
-            })}
-          </tbody> */}
+          <tbody>
+            <tr>
+              <th> {this.props.data[0].date} </th>
+              <th> {this.props.data[0].liftname} </th>
+              <th> {this.props.data[0].weightlifted} </th>
+              <th> {this.props.data[0].repsperformed} </th>
+              <th> {this.props.data[0].onerm} </th>
+
+              {/* {lifts[0].map(lift => {
+                return <th key={lift.id}>{lift}</th>;
+              })} */}
+
+              {/* {this.props.data.map(function(lift) {
+                return React.createElement('th', { key: lift.id }, lift);
+              })} */}
+            </tr>
+          </tbody>
         </table>
       </div>
     );
   }
 }
+
+Lifts.defaultProps = {
+  lifts: []
+};
+
+// {
+//   Array.from(this.state.lifts).map(function(lift) {
+//     return React.createElement(Lift, { key: lift.id, lift: lift });
+//   });
+// }
+
+// {this.state.lifts.map(lift => {
+//   return <th key={lift.id}> {lift} </th>;
+// })}
 
 // Array.from(this.state.lifts).map(function(lift) {
 //   return React.createElement(Lift, { key: lift.id, lift: lift });
